b2967d83fa6003f36552941a46871dc7ae55c190
yu1git <p412elmd-b6gjt@yahoo.co.jp>
2021-09-15 15:13:35 +0900

    game

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1 class="bg-info text-white display-4 px-3">〇✕ゲーム</h1>
+  <div id="app" class="continer  mx-auto mt-4 board" style="width:450px">
+    <Masu  v-for='n of 9' v-bind:key='n' v-bind:count="count" v-on:countadd="countadd" />
+
+  </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Masu from "./components/masu.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Masu
+  },
+  data(){
+     return{
+       count:0
+     }
+  },
+  methods: {
+    countadd(){
+      this.count++
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.board{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
 }
 </style>
---
+++ src/components/masu.vue
@@ -0,0 +1,46 @@
+<template>
+  
+  <div class="waku" v-on:click="change" >{{hyouji}}</div>
+</template>
+
+<script>
+export default {
+    name:"Masu",
+    props:{
+      count:Number
+    },
+    data(){
+      return{
+        hyouji:"",
+      }
+    },
+    methods: {
+      change(){
+        if(this.count%2===0){
+        this.hyouji="〇";
+        // this.count++; countは親で管理する
+        this.$emit('countadd',this.count)
+        console.log(this.count);
+        }else{
+          this.hyouji="×";
+          // this.count++
+        this.$emit('countadd',this.count)
+
+          console.log(this.count);
+
+        }
+      }
+    }
+}
+</script>
+
+<style>
+.waku {
+  height: 150px;
+  width: 150px;
+  border: 1px solid black;
+  line-height: 150px;
+  font-size: 90px;
+  text-align: center;
+}
+</style>
